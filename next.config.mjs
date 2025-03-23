dc21ef9942b631ae3bcb7bf1871f3a1baa6062c4
Gokul Kiran R <gokul.kiran03@gmail.com>
2025-03-24 01:09:50 +0530

    next json update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    domains: ['localhost'],
+    unoptimized: true
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+};
 
-export default nextConfig;
+export default nextConfig; 
