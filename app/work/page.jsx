ab778ad698b1d85da22562ce3bdd6867652acac0
Gokul Kiran R <gokul.kiran03@gmail.com>
2025-04-10 11:03:53 +0530

    projects added

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -29,6 +29,18 @@ const projects =[
     },
     {
         num: '02',
+        category: 'AI|ML',
+        title: 'Streamlining Hospital Waste Management',
+        description:
+            "A deep learning-based system to classify hospital waste into organic and inorganic categories for efficient and eco-friendly disposal. The system uses image processing techniques to analyze waste images and classify them accordingly.",
+        stack: [
+            {name: "Python"}, {name: "Flask"}, {name: "Html"}, {name: "Css"}, {name: "JS"}, {name: "Tensorflow"}, {name: "Keras"}],
+        image: '/Images/project5.png',
+        live: "",
+        github: "https://github.com/GKRBROS/STREAMLINING-HOSPITAL-WASTE-MANAGEMENT.git",
+    },
+    {
+        num: '03',
         category: 'Bot',
         title: 'Dewz Discord Bot',
         description:
@@ -40,7 +52,19 @@ const projects =[
         github: "https://github.com/GKRBROS/TCL_PYTHON_PROJECT.git",
     },
     {
-        num: '03',
+        num: '04',
+        category: 'Website',
+        title: 'Brahma25',
+        description:
+            "Developed a website for for college fest techno-cultural fest 'Brahma25'. The website includes all the details about the fest, including events, registration, and more.",
+        stack: [
+            {name: "Html"}, {name: "Css"}, {name: "JS"}, {name: "API"}],
+        image: '/Images/project6.png',
+        live: "https://brahma25.live/",
+        github: "https://github.com/GKRBROS/Brahma25.git",
+    },
+    {
+        num: '05',
         category: 'Game',
         title: 'FPS Shooting Game',
         description:
@@ -52,7 +76,7 @@ const projects =[
         github: "https://github.com/GKRBROS/Game_Development.git",
     },
     {
-        num: '04',
+        num: '06',
         category: '3D Model',
         title: '3D Model Design',
         description:
